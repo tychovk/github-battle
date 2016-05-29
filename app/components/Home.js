f3c9aab2c4a9c0d23dec3f028066cd580fbe1888
tychovk <tychovk@gmail.com>
2016-05-28 19:18:54 -0500

    files from woopwoop

---
+++ app/components/Home.js
@@ -0,0 +1,21 @@
+//app/components/Home.js
+var React = require('react')
+var transparentBg = require('../styles').transparentBg
+var ReactRouter = require('react-router')
+var Link = ReactRouter.Link
+
+var Home = React.createClass({
+    render () {
+        return (
+            <div className='jumbotron col-sm-12 text-center'  style={transparentBg}>
+                <h1>Github Battle</h1>
+                <p className='lead'>Some fancy motto.</p>
+                <Link to='/playerOne'>
+                    <button type='button' className='btn btn-l btn-success'>Get Stated</button>
+                </Link>
+            </div>
+        )
+    }
+})
+
+module.exports = Home
